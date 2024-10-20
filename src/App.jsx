b5dc891bf15bc4a1eee875c08edfc2a8aedec08e
Joseph Emeka Kamange <kamjojo111@gmail.com>
2024-10-21 00:44:14 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Body from "./Component/Body"
+import Footer from "./Component/Footer"
+import Header from "./Component/Header"
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css"
+function Footer () {
+    return(
+        <div className="icons">
+            <div>
+        
+        </div>
+        </div>
+    )
+}
+
+export default Footer
